492beb44f875308939f019dd91e42c5396a6b776
Tatsuya Tanaka <kanatatsu64@gmail.com>
2024-09-14 16:03:52 +0900

    check iconを追加

---
+++ frontend/src/components/icon/CheckIcon.tsx
@@ -0,0 +1,6 @@
+import { Icon } from "@chakra-ui/react";
+import { FaCheckCircle } from "react-icons/fa";
+
+export const CheckIcon: React.FC = () => (
+  <Icon color="primary.main" as={FaCheckCircle} />
+);
