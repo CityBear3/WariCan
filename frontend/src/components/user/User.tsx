565c5ee150fdf36db9d30e8e4e63bbbaed766980
Tatsuya Tanaka <kanatatsu64@gmail.com>
2024-09-14 21:39:42 +0900

    Userを修正

--- frontend/src/components/user/User.tsx
+++ frontend/src/components/user/User.tsx
@@ -1,21 +1,18 @@
 import { UserModel } from "@/domain/user";
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
 import { UserImage } from "./UserImage";
+import { UserName } from "./UserName";
 
 type Props = {
   user: UserModel;
   withIcon?: "check" | "pending";
 };
 
-const textProps = {
-  fontSize: "24px",
-};
-
 export const User: React.FC<Props> = ({ user, withIcon }) => {
   return (
     <HStack>
       <UserImage user={user} withIcon={withIcon} />
-      <Text {...textProps}>{user.name}</Text>
+      <UserName user={user} nameSize="24px" titleSize="24px" />
     </HStack>
   );
 };
