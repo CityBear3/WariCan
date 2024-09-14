b511acbc38fa055d6b9dc04f907f9735f97e4b83
Tatsuya Tanaka <kanatatsu64@gmail.com>
2024-09-14 19:24:57 +0900

    ロゴを追加

---
+++ frontend/src/components/text/Logo.tsx
@@ -0,0 +1,15 @@
+import { Text } from "@chakra-ui/react";
+import { ComponentProps } from "react";
+
+const logoProps = {
+  fontSize: "32px",
+  fontWeight: "bold",
+};
+
+type Props = {} & ComponentProps<typeof Text>;
+
+export const Logo: React.FC<Props> = ({ ...props }) => (
+  <Text {...logoProps} {...props}>
+    WARICAN
+  </Text>
+);
