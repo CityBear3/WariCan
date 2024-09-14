8775a52b128f0ceecfac4f7e33a5a4467ee3d619
Tatsuya Tanaka <kanatatsu64@gmail.com>
2024-09-14 17:02:26 +0900

    user imageを追加

---
+++ frontend/src/components/user/UserImage.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { UserModel } from "@/domain/user";
+import { UserImage } from "./UserImage";
+
+const meta: Meta<typeof UserImage> = {
+  component: UserImage,
+};
+
+export default meta;
+type Story = StoryObj<typeof UserImage>;
+
+const user: UserModel = {
+  id: "sample id",
+  name: "John Smith",
+  imageUrl: "http://localhost:3000/sample_profile.png",
+  tag: "#camp\n#BBQ",
+};
+
+export const Normal: Story = {
+  args: {
+    user,
+  },
+};
+
+export const Check: Story = {
+  args: {
+    user,
+    withIcon: "check",
+  },
+};
+
+export const Pending: Story = {
+  args: {
+    user,
+    withIcon: "pending",
+  },
+};
