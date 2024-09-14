9901d20c1438722a460dd849c38fd85817541579
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2024-09-15 08:37:15 +0900

    feat: serverを仮実装

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/rs/cors"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "localhost"
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	//interceptors := connect.WithInterceptors(
+	//	interceptor.NewAppContextInterceptor(),
+	//	interceptor.NewRequestLogInterceptor(),
+	//)
+
+	//walletPath, walletHandler := walletApiconnect.NewWalletHandler()
+
+	mux := http.NewServeMux()
+
+	//mux.Handle(walletPath, walletHandler)
+
+	svr := http.Server{
+		Addr: fmt.Sprintf("%s:%s", host, port),
+		Handler: cors.AllowAll().Handler(
+			h2c.NewHandler(mux, &http2.Server{}),
+		),
+	}
+
+	log.Println("server start on port")
+	if err := svr.ListenAndServe(); err != nil {
+		panic(err)
+	}
+
+	waitSIGINT()
+	defer func() {
+		if err := svr.Shutdown(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
+}
+
+func waitSIGINT() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	signal.Stop(quit)
+	close(quit)
 }
