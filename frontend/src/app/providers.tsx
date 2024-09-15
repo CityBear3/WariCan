16059f95994b55e44e0b29ced3426ffb50a5b497
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2024-09-15 16:52:32 +0900

    feat: uuidを追加

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -5,14 +5,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { TransportProvider } from "@connectrpc/connect-query";
 import { createConnectTransport } from "@connectrpc/connect-web";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { v4 as uuidV4 } from "uuid";
 
 const finalTransport = createConnectTransport({
   baseUrl: "http://localhost:8080",
   interceptors: [
     (next) => (request) => {
       request.header.append(
-        "X-Idempotency-Key",
-        "34052303-e518-4899-8522-4ff72d6bbe32"
+        "X-Idempotency-Key", uuidV4()
       );
       return next(request);
     },
