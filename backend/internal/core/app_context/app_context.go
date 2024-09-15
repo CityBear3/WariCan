1f86adb82018cddbbaa97e5df3ffe6843fad1296
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2024-09-15 11:58:06 +0900

    feat: interceptorを追加

---
+++ backend/internal/core/app_context/app_context.go
@@ -0,0 +1,37 @@
+package app_context
+
+import (
+	"context"
+	"errors"
+	"github.com/CityBear3/WariCan/internal/domain/user"
+	"github.com/google/uuid"
+	"time"
+)
+
+const AppContextKey = "AppContext"
+
+var ErrAppContextNotFound = errors.New("app context not found")
+
+func GetAppContext(ctx context.Context) (AppContext, error) {
+	appContext, ok := ctx.Value(AppContextKey).(AppContext)
+	if !ok {
+		return AppContext{}, ErrAppContextNotFound
+	}
+	return appContext, nil
+}
+
+type AppContext struct {
+	IdempotencyKey uuid.UUID
+	CallUUID       uuid.UUID
+	UserID         user.ID
+	CurrentTime    time.Time
+}
+
+func NewAppContext(idempotencyKey uuid.UUID, callUUID uuid.UUID, userID user.ID, currentTime time.Time) AppContext {
+	return AppContext{
+		IdempotencyKey: idempotencyKey,
+		CallUUID:       callUUID,
+		UserID:         userID,
+		CurrentTime:    currentTime,
+	}
+}
