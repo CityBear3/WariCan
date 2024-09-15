4abca25e86f69854186fe6ac1b67bcfa1add7b73
Tatsuya Tanaka <kanatatsu64@gmail.com>
2024-09-16 02:58:50 +0900

    loginを修正

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { PrimaryButton } from "@/components/button/PrimaryButton";
 import { TextField } from "@/components/input/TextField";
 import { Section } from "@/components/layout/Section";
 import { VStack } from "@chakra-ui/react";
+import { FormProvider, useForm } from "react-hook-form";
 
 const sectionProps = {
   width: "85%",
@@ -19,18 +22,42 @@ const buttonProps = {
   padding: "15px",
 };
 
+type FormValues = {
+  email: string;
+  password: string;
+};
+
 const Login: React.FC = () => {
+  const methods = useForm<FormValues>();
+
+  const onSubmit = (values: FormValues) => {
+    alert([values.email, values.password]);
+  };
+
   return (
-    <Section title="ログイン" {...sectionProps}>
-      <VStack width="80%" margin="auto">
-        <TextField
-          placeholder="メールアドレスを入力します"
-          {...textFieldProps}
-        />
-        <TextField placeholder="パスワードを入力します" {...textFieldProps} />
-        <PrimaryButton label="ログインする" {...buttonProps} />
-      </VStack>
-    </Section>
+    <FormProvider {...methods}>
+      <Section title="ログイン" {...sectionProps}>
+        <VStack width="80%" margin="auto">
+          <TextField
+            placeholder="メールアドレスを入力します"
+            name="email"
+            type="email"
+            {...textFieldProps}
+          />
+          <TextField
+            placeholder="パスワードを入力します"
+            name="password"
+            type="password"
+            {...textFieldProps}
+          />
+          <PrimaryButton
+            label="ログインする"
+            onClick={methods.handleSubmit(onSubmit)}
+            {...buttonProps}
+          />
+        </VStack>
+      </Section>
+    </FormProvider>
   );
 };
 
