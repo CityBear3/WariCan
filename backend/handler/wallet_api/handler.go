1d2d5dc6f132b8325004b98c56094e8ebfb86623
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2024-09-15 15:46:01 +0900

    feat: DepositV1にloggerを追加

--- backend/handler/wallet_api/handler.go
+++ backend/handler/wallet_api/handler.go
@@ -26,7 +26,7 @@ func NewHandler(walletAppService *wallet_app_service.Service) *Handler {
 func (h Handler) DepositV1(ctx context.Context, c *connect.Request[walletApi.WalletDepositV1_Request]) (*connect.Response[walletApi.WalletDepositV1_Response], error) {
 	actx, err := app_context.GetAppContext(ctx)
 	if err != nil {
-		log.Panicf("failed to get app context: %v\n", err)
+		log.Printf("failed to get app context: %v\n", err)
 		return nil, connectrpc.CreateErrorResponse(err)
 	}
 
